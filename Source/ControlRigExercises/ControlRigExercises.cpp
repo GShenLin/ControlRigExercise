1e44bdcfaae2a1c56a2fe0fade13d3af7d227f99
GSL <3303452085@qq.com>
2022-04-19 13:54:06 +0800

    Init

---
+++ Source/ControlRigExercises/ControlRigExercises.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ControlRigExercises.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ControlRigExercises, "ControlRigExercises" );
